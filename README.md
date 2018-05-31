98aa3947939a2c4a0699665849b5ede04f926e50
nctl144 <nctl144@gmail.com>
2018-05-31 14:28:42 -0400

    update parsejson.py and readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # parse-correctness
 
 + parsejson.py will generate the url files for Rust project!
+
+Test implemented:
++ Rust scheme, netloc
++ urllib join, scheme, netloc
--- parsejson.py
+++ parsejson.py
@@ -21,7 +21,9 @@ def write_rust(path, input_list):
 
 def main():
 
-    # delete generated files if presented
+    '''
+    delete generated files if presented
+    '''
     workpath = os.path.join(os.getcwd())
     remove_files(workpath, "input-url.txt")
     remove_files(workpath, "scheme-url.txt")
@@ -29,28 +31,39 @@ def main():
     rust_workpath = os.path.join(os.getcwd(), "urlparsetest")
     remove_files(rust_workpath, "input-url.txt")
     remove_files(rust_workpath, "scheme-url.txt")
+    remove_files(rust_workpath, "netloc-url.txt")
 
     with open("urltest.json", 'r') as f:
         datastore = json.load(f)
 
+    '''
+    init the arrays for the txt files
+    '''
     result_url_list = []
     scheme_list = []
+    netloc_list = []
 
     for item in datastore:
         result_url, scheme = '', ''
 
+        '''
+        add the items from the json file to the arrays
+        '''
         if 'href' in item:
             result_url = item['href']
             scheme = item['protocol']
             scheme = scheme[:-1] if scheme[-1] == ':' else scheme
+            netloc = item['hostname']
 
-        # if result_url is not empty -> scheme is not empty
         if result_url.strip() != '':
             result_url_list.append(result_url)
             scheme_list.append(scheme)
+            netloc_list.append(netloc)
 
 
-
+    '''
+    Write the result to text files
+    '''
     url_output_file = open("input-url.txt", "w")
     for url in result_url_list:
         url_output_file.write("{0}\n".format(url))
@@ -64,6 +77,7 @@ def main():
     # remove the white line at the end for rust
     write_rust("urlparsetest/input-url.txt", result_url_list)
     write_rust("urlparsetest/scheme-url.txt", scheme_list)
+    write_rust("urlparsetest/netloc-url.txt", netloc_list)
 
 
 if __name__ == "__main__":
