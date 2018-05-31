143e81b24bbf356b54908468e50cf3461e24341d
nctl144 <nctl144@gmail.com>
2018-05-31 11:24:45 -0400

    add rust project, add main func python

--- parsejson.py
+++ parsejson.py
@@ -2,43 +2,49 @@ import json
 import os
 
 
-# delete generated files if presented
-workpath = os.path.join(os.getcwd())
+def main():
 
-if os.path.exists(os.path.join(workpath, "input-url.txt")):
-    os.remove(os.path.join(workpath, "input-url.txt"))
+    # delete generated files if presented
+    workpath = os.path.join(os.getcwd())
 
-if os.path.exists(os.path.join(workpath, "scheme-url.txt")):
-    os.remove(os.path.join(workpath, "scheme-url.txt"))
+    if os.path.exists(os.path.join(workpath, "input-url.txt")):
+        os.remove(os.path.join(workpath, "input-url.txt"))
 
-
-with open("urltest.json", 'r') as f:
-    datastore = json.load(f)
-
-result_url_list = []
-scheme_list = []
-
-for item in datastore:
-    result_url, scheme = '', ''
-
-    if 'href' in item:
-        result_url = item['href']
-        scheme = item['protocol']
-        scheme = scheme[:-1] if scheme[-1] == ':' else scheme
-
-    # if result_url is not empty -> scheme is not empty
-    if result_url.strip() != '':
-        result_url_list.append(result_url)
-        scheme_list.append(scheme)
+    if os.path.exists(os.path.join(workpath, "scheme-url.txt")):
+        os.remove(os.path.join(workpath, "scheme-url.txt"))
 
 
+    with open("urltest.json", 'r') as f:
+        datastore = json.load(f)
 
-url_output_file = open("input-url.txt", "w")
-for url in result_url_list:
-    url_output_file.write("{0}\n".format(url))
-url_output_file.close()
+    result_url_list = []
+    scheme_list = []
 
-scheme_output_file = open("scheme-url.txt", "w")
-for scheme in scheme_list:
-    scheme_output_file.write("{0}\n".format(scheme))
-scheme_output_file.close()
+    for item in datastore:
+        result_url, scheme = '', ''
+
+        if 'href' in item:
+            result_url = item['href']
+            scheme = item['protocol']
+            scheme = scheme[:-1] if scheme[-1] == ':' else scheme
+
+        # if result_url is not empty -> scheme is not empty
+        if result_url.strip() != '':
+            result_url_list.append(result_url)
+            scheme_list.append(scheme)
+
+
+
+    url_output_file = open("input-url.txt", "w")
+    for url in result_url_list:
+        url_output_file.write("{0}\n".format(url))
+    url_output_file.close()
+
+    scheme_output_file = open("scheme-url.txt", "w")
+    for scheme in scheme_list:
+        scheme_output_file.write("{0}\n".format(scheme))
+    scheme_output_file.close()
+
+
+if __name__ == "__main__":
+    main()
--- python/urllibtest.py
+++ python/urllibtest.py
@@ -4,67 +4,73 @@ import json
 import collections
 
 
-with open("../urltest.json", 'r') as f:
-    datastore = json.load(f)
+def main():
 
-incorrect_url = collections.defaultdict(list)
+    with open("../urltest.json", 'r') as f:
+        datastore = json.load(f)
 
-for item in datastore:
-    input_url = item['input']
-    base_url = item['base']
+    incorrect_url = collections.defaultdict(list)
 
-    if base_url == 'about:blank':
-        base_url = ''
+    for item in datastore:
+        input_url = item['input']
+        base_url = item['base']
 
-    scheme, netloc, path, search, failure, result_url = '', '', '', '', False, ''
+        if base_url == 'about:blank':
+            base_url = ''
 
-    if 'href' in item:
-        result_url = item['href']
-        result_url = result_url[:-1] if result_url[-1] == '/' else result_url
+        scheme, netloc, path, search, failure, result_url = '', '', '', '', False, ''
 
-    if 'protocol' in item:
-        scheme = item['protocol']
-        scheme = scheme[:-1] if scheme[-1] == ':' else scheme
+        if 'href' in item:
+            result_url = item['href']
+            result_url = result_url[:-1] if result_url[-1] == '/' else result_url
 
-    if 'hostname' in item:
-        netloc = item['hostname']
+        if 'protocol' in item:
+            scheme = item['protocol']
+            scheme = scheme[:-1] if scheme[-1] == ':' else scheme
 
-    if 'pathname' in item:
-        path = item['pathname']
+        if 'hostname' in item:
+            netloc = item['hostname']
 
-    if 'search' in item:
-        query = item['search']
+        if 'pathname' in item:
+            path = item['pathname']
 
-    if 'failure' in item:
-        failure = True
+        if 'search' in item:
+            query = item['search']
 
-    try:
-        join_url = urljoin(base_url, input_url)
-        if len(join_url) > 1:
-            join_url = join_url[:-1] if join_url[-1] == '/' else join_url
+        if 'failure' in item:
+            failure = True
+
+        try:
+            join_url = urljoin(base_url, input_url)
+            if len(join_url) > 1:
+                join_url = join_url[:-1] if join_url[-1] == '/' else join_url
 
 
-        if result_url.lower() != '' and result_url.lower() != join_url.lower():
+            if result_url.lower() != '' and result_url.lower() != join_url.lower():
+                if not failure:
+                    incorrect_url[input_url].append("incorrect join url")
+
+
+            parsed_obj = urlparse(join_url)
+
+            if scheme.lower() != '' and parsed_obj.scheme.lower() != scheme.lower():
+                if not failure:
+                    incorrect_url[input_url].append("incorrect scheme")
+
+            if netloc.lower() != '' and parsed_obj.netloc.lower() != netloc.lower():
+                if not failure:
+                    incorrect_url[input_url].append("incorrect netloc")
+
+
+        except ValueError:
             if not failure:
-                incorrect_url[input_url].append("incorrect join url")
-
-
-        parsed_obj = urlparse(join_url)
-
-        if scheme.lower() != '' and parsed_obj.scheme.lower() != scheme.lower():
-            if not failure:
-                incorrect_url[input_url].append("incorrect scheme")
-
-        if netloc.lower() != '' and parsed_obj.netloc.lower() != netloc.lower():
-            if not failure:
-                incorrect_url[input_url].append("incorrect netloc")
-
-
-    except ValueError:
-        if not failure:
-            print("the value error url is:", input_url)
+                print("the value error url is:", input_url)
 
 
 
-for key, val in incorrect_url.items():
-    print(key, '----', val)
+    for key, val in incorrect_url.items():
+        print(key, '----', val)
+
+
+if __name__ == "__main__":
+    main()
