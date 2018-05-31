186e127d61806ea1513da4819f0a24f8bb962cdd
nctl144 <nctl144@gmail.com>
2018-05-31 15:09:46 -0400

    read the scheme input

--- c/uriparsertest.c
+++ c/uriparsertest.c
@@ -6,18 +6,7 @@
 #include <time.h>
 
 int main() {
-    UriParserStateA state;
-    UriUriA uri;
 
-
-    state.uri = &uri;
-
-    if (uriParseUriA(&state, "http://банки.рф") != URI_SUCCESS) {
-       uriFreeUriMembersA(&uri);
-    }
-
-    uriFreeUriMembersA(&uri);
-    // uriFreeUriMembersA(&uri);
     // printf("absolutePath: %d\n", uri.absolutePath);
     // printf("scheme after Last: %s\n", uri.scheme.afterLast);
     // printf("scheme first: %s\n", uri.scheme.first);
@@ -27,8 +16,8 @@ int main() {
     // printf("hostData ip6: %s\n", uri.hostData.ip6->data);
     // printf("hostData ipFuture afterLast: %s\n", uri.hostData.ipFuture.afterLast);
     // printf("hostData ipFuture first: %s\n", uri.hostData.ipFuture.first);
-    printf("host text after Last: %s\n", uri.hostText.afterLast);
-    printf("host text first: %s\n", uri.hostText.first);
+    // printf("host text after Last: %s\n", uri.hostText.afterLast);
+    // printf("host text first: %s\n", uri.hostText.first);
     // printf("owner: %d\n", uri.owner);
     //
     // // ask about this later
@@ -40,37 +29,47 @@ int main() {
     // printf("query afterLast: %s\n", uri.query.afterLast);
     // printf("query First: %s\n", uri.query.first);
 
+    FILE* scheme_url;
 
-    // double time_elapsed;
-    //
-    // FILE* fp;
-    // char buffer[255];
-    //
-    // fp = fopen("chromiumUrls.txt", "r");
-    //
-    // while (fgets(buffer, 255, (FILE*) fp)) {
-    //     clock_t start, end;
-    //
-    //     UriParserStateA state;
-    //     UriUriA uri;
-    //     // start
-    //     start = clock();
-    //
-    //     state.uri = &uri;
-    //
-    //     if (uriParseUriA(&state, buffer) != URI_SUCCESS) {
-    //         uriFreeUriMembersA(&uri);
-    //     }
-    //
-    //     uriFreeUriMembersA(&uri);
-    //     end = clock();
-    //
-    //     time_elapsed += ((double) (end - start) / CLOCKS_PER_SEC);
-    //
-    // }
-    //
-    // fclose(fp);
-    //
-    // printf("time elapsed for this program is: %f\n", time_elapsed);
-    // return 0;
+    char scheme_url_buffer[255];
+    char scheme_list[409][255];
+
+    scheme_url = fopen("scheme-url.txt", "r");
+    int index = 0;
+    while (fgets(scheme_url_buffer, 255, (FILE*) scheme_url)) {
+        strcpy(scheme_list[index], scheme_url_buffer);
+
+        index += 1;
+    }
+
+    int i;
+
+    for (i = 0; i < 409; i++) {
+        printf("%s", scheme_list[i]);
+    }
+
+    fclose(scheme_url);
+
+    FILE* input_url;
+
+    char input_url_buffer[255];
+    input_url = fopen("input-url.txt", "r");
+    while (fgets(input_url_buffer, 255, (FILE*) input_url)) {
+        // UriParserStateA state;
+        // UriUriA uri;
+
+        // state.uri = &uri;
+        //
+        // if (uriParseUriA(&state, input_url_buffer) != URI_SUCCESS) {
+        //     uriFreeUriMembersA(&uri);
+        // }
+        //
+        // uriFreeUriMembersA(&uri);
+
+    }
+
+    fclose(input_url);
+
+    printf("the program has finished :)");
+    return 0;
 }
