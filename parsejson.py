d490c472c17c1468426f75ba66b2688ff532ff06
nctl144 <nctl144@gmail.com>
2018-06-01 11:22:01 -0400

    add path input

--- parsejson.py
+++ parsejson.py
@@ -28,21 +28,25 @@ def main():
     remove_files(c_workpath, "input-url.txt")
     remove_files(c_workpath, "scheme-url.txt")
     remove_files(c_workpath, "netloc-url.txt")
+    remove_files(c_workpath, "path-url.txt")
 
     python_workpath = os.path.join(os.getcwd(), "python")
     remove_files(python_workpath, "input-url.txt")
     remove_files(python_workpath, "scheme-url.txt")
     remove_files(python_workpath, "netloc-url.txt")
+    remove_files(python_workpath, "path-url.txt")
 
     gurlcython_workpath = os.path.join(os.getcwd(), "gurl-cython")
     remove_files(gurlcython_workpath, "input-url.txt")
     remove_files(gurlcython_workpath, "scheme-url.txt")
     remove_files(gurlcython_workpath, "netloc-url.txt")
+    remove_files(gurlcython_workpath, "path-url.txt")
 
     rust_workpath = os.path.join(os.getcwd(), "urlparsetest")
     remove_files(rust_workpath, "input-url.txt")
     remove_files(rust_workpath, "scheme-url.txt")
     remove_files(rust_workpath, "netloc-url.txt")
+    remove_files(rust_workpath, "path-url.txt")
 
     with open("urltest.json", 'r') as f:
         datastore = json.load(f)
@@ -53,9 +57,10 @@ def main():
     result_url_list = []
     scheme_list = []
     netloc_list = []
+    path_list = []
 
     for item in datastore:
-        result_url, scheme = '', ''
+        result_url, scheme, netloc, path = '', '', '', ''
 
         '''
         add the items from the json file to the arrays
@@ -65,11 +70,14 @@ def main():
             scheme = item['protocol']
             scheme = scheme[:-1] if scheme[-1] == ':' else scheme
             netloc = item['hostname']
+            path = item['pathname']
+
 
         if result_url.strip() != '':
             result_url_list.append(result_url)
             scheme_list.append(scheme)
             netloc_list.append(netloc)
+            path_list.append(path)
 
 
     '''
@@ -78,18 +86,22 @@ def main():
     write_result("python/input-url.txt", result_url_list)
     write_result("python/scheme-url.txt", scheme_list)
     write_result("python/netloc-url.txt", netloc_list)
+    write_result("python/path-url.txt", path_list)
 
     write_result("gurl-cython/input-url.txt", result_url_list)
     write_result("gurl-cython/scheme-url.txt", scheme_list)
     write_result("gurl-cython/netloc-url.txt", netloc_list)
+    write_result("gurl-cython/path-url.txt", path_list)
 
     write_result("c/input-url.txt", result_url_list)
     write_result("c/scheme-url.txt", scheme_list)
     write_result("c/netloc-url.txt", netloc_list)
+    write_result("c/path-url.txt", path_list)
 
     write_result("urlparsetest/input-url.txt", result_url_list)
     write_result("urlparsetest/scheme-url.txt", scheme_list)
     write_result("urlparsetest/netloc-url.txt", netloc_list)
+    write_result("urlparsetest/path-url.txt", path_list)
 
 
 if __name__ == "__main__":
