cd062da121d141cd05e95fbc2e43c437c22e743d
nctl144 <nctl144@gmail.com>
2018-06-04 10:30:28 -0500

    update the test for gurl

--- gurl-cython/gurl-cython-test.py
+++ gurl-cython/gurl-cython-test.py
@@ -25,25 +25,25 @@ def main():
 
     for url in input_list:
         parsed_result = URL(url.encode())
-        path_result = parsed_result.path().decode()
+        scheme_result = parsed_result.scheme().decode()
 
-        if scheme_list[counter_scheme] != parsed_result.scheme().decode():
+        if scheme_list[counter_scheme] != scheme_result:
             print("unmatched scheme at", url)
 
         counter_scheme += 1
 
     print('\n')
-    
-    for url in input_list:
-        parsed_result = URL(url.encode())
-        path_result = parsed_result.path().decode()
 
-        if netloc_list[counter_netloc] != parsed_result.host().decode():
-            print("unmatched netloc at", url)
-
-        counter_netloc += 1
-
-    print('\n')
+    # for url in input_list:
+    #     parsed_result = URL(url.encode())
+    #     netloc_result = parsed_result.host().decode()
+    #
+    #     if netloc_list[counter_netloc] != netloc_result:
+    #         print("unmatched netloc at", url, "the result is", netloc_result, "while it should be", netloc_list[counter_netloc])
+    #
+    #     counter_netloc += 1
+    #
+    # print('\n')
 
     for url in input_list:
         parsed_result = URL(url.encode())
@@ -53,7 +53,7 @@ def main():
             if ((path_result == "" and path_list[counter_path] == '/') or (path_result == "/" and path_list[counter_path] == "")):
                 pass
             else:
-                print("unmatched path at", url)
+                print("unmatched path at", url, "the result is", path_result, "while it should be", path_list[counter_path])
 
         counter_path += 1
 
